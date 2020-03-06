3bc7c166d55e7f05074b825fd47574ec7f9de42b
Aaron Dalton <aadalton@vt.edu>
2020-03-05 21:57:06 -0500

    Update picture galery

--- js/scripts.js
+++ js/scripts.js
@@ -184,23 +184,23 @@ $(document).ready(function () {
         },
         data: {
             // Event title
-            title: "Aaron and Rache's wedding",
+            title: "Aaron and Rachel's wedding",
 
             // Event start date
-            start: new Date('Nov 27, 2017 10:00'),
+            start: new Date('June 7, 2020 17:00'),
 
             // Event duration (IN MINUTES)
             // duration: 120,
 
             // You can also choose to set an end time
             // If an end time is set, this will take precedence over duration
-            end: new Date('Nov 29, 2017 00:00'),
+            end: new Date('June 7, 2020 23:20'),
 
             // Event Address
-            address: 'ITC Fortune Park Hotel, Kolkata',
+            address: '7300 Macarthur Blvd, Glen Echo, MD 20812',
 
             // Event Description
-            description: "We can't wait to see you on our big day. For any queries or issues, please contact Mr. Amit Roy at +91 9876543210."
+            description: "We can't wait to see you on our big day. For any queries or issues, please contact Dr. James Dalton at 540-815-9828."
         }
     });
 
